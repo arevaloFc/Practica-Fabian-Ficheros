8fcf306f27b5f1026cb96dda6554351df8d95fd7
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-06-15 22:24:26 +0100

    Refactor: Se elimino los daotos del Plane y se cambio a null en la clase Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         boolean salir = false;
-        Plane F18 = new Plane(34f,"camilo","R-red2");
+        Plane F18 = null;
 
         Scanner sc = new Scanner(System.in);
         String opcionUsuario = "";
